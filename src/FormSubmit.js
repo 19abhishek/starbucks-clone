38887644c167150d9ec244e1003185d52ff2a6ef
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-20 23:15:12 +0530

    Login Screen footer and styling completed

--- src/FormSubmit.js
+++ src/FormSubmit.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./FormSubmit.css";
 
-function FormSubmit() {
-  return <div></div>;
+function FormSubmit({ name, type, onClick }) {
+  return (
+    <button className="formSubmit" type={type}>
+      {name}
+    </button>
+  );
 }
-
 export default FormSubmit;
