a9e585cb6c02c270ad40fff6d570f41a2bca975f
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-21 22:12:37 +0530

    Logout functionality added

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -7,7 +7,7 @@ function MenuItem({ type, image }) {
   return (
     <Fade>
       <Link className="menuItem">
-        <img src={image} alt="" className="menuItem__image" />
+        <img src={image} alt={type} className="menuItem__image" />
         <h4 className="menuItem__type">{type}</h4>
       </Link>
     </Fade>
